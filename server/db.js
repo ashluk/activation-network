6bad3dd805a2c82bbd3f21315e60955a2cbf4b19
Ash Gowriluk <ashleerluk@gmail.com>
2021-04-25 19:49:52 +0200

    heroku pg update

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const spicedPg = require("spiced-pg");
 
 const db = spicedPg(
     process.env.DATABASE_URL ||
-        "postgres:postgres:postgres@localhost:5432/finalproject"
+        "postgres://fhodeumjqqupdn:623941877d65c5ed49830332b4cd9b76e61c154b9e0f4149371a6aa8a8a5a805@ec2-54-74-156-137.eu-west-1.compute.amazonaws.com:5432/ddg9dtsn7ovsjm"
 );
 //////////////////REGISTER//////////////////////////
 module.exports.addUser = (first, last, email, password, artistormusician) => {
