141f2501e336e784b55b4535b59ec57d2f8d6840
Ash Gowriluk <ashleerluk@gmail.com>
2021-04-26 17:05:49 +0200

    more socket changes

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ const s3 = require("./s3");
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {
     allowRequest: (req, callback) =>
-        callback(null, req.headers.referer.startsWith()),
+        callback(
+            null,
+            req.headers.referer.startsWith(
+                "https://activationnetwork.herokuapp.com/"
+            )
+        ),
 });
 
 const cryptoRandomString = require("crypto-random-string");
